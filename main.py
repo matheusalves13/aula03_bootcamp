b405f0477ab8768685065d36d7fbb4f4ed2258a2
Matheus da Silva <matheus.akurama@gmail.com>
2025-08-12 11:49:52 -0300

    adicionando main com exemplos

---
+++ main.py
@@ -0,0 +1,14 @@
+# x=-3
+
+# if x<=2:
+#         print('é menor do que zero')
+# elif x<=00:
+#         print('é menor do que dois')
+# else:
+#         print('more')
+
+lista_de_alunos = ["rafael", "fabio", "luciano"]
+
+
+for aluno in lista_de_alunos:
+        print(aluno)
